d61ed91d4dafccc53ec733c01bd2cf18a59a628f
IGNP Andre Pratama Milano <andremilano16@gmail.com>
2025-08-06 10:39:59 +0700

    add: Qualification menu testing

--- cypress/e2e/Admin-Qualification-Licenses.cy.js
+++ cypress/e2e/Admin-Qualification-Licenses.cy.js
@@ -2,35 +2,34 @@
 
 
 // job
-describe('Qualification Skills', () => {
+describe('Qualification Education', () => {
 
   beforeEach(() => {
     cy.login('Admin', 'admin123')
   })
 
   it('General', () => {
-    cy.visit('/web/index.php/admin/viewSkills')
-    cy.get('.oxd-text').should('contain', 'Skills')
+    cy.visit('/web/index.php/admin/viewLicenses')
+    cy.get('.oxd-text').should('contain', 'Licenses')
     cy.get('.oxd-text').should('be.visible')
   })
 
   // add Skill
-  it('add Skill', () => {
-    cy.visit('/web/index.php/admin/viewSkills')
+  it('add License', () => {
+    cy.visit('/web/index.php/admin/viewLicenses')
     cy.get('.oxd-button').click()
-    cy.get('.oxd-input').eq(1).type('Test Skill Name')
-    cy.get('.oxd-textarea').eq(0).type('Test Skill Description')
+    cy.get('.oxd-input').eq(1).type('Test Licenses Name')
     cy.get('.oxd-button--secondary').contains('Save').click()
     cy.wait(3000) // wait for the save to complete
   })
 
   // edit & remove Skill
-  it('edit and remove Skill', () => {
-    cy.visit('/web/index.php/admin/viewSkills')
+  it('edit and remove Licenses', () => {
+    cy.visit('/web/index.php/admin/viewLicenses')
 
     // edit the Skill
     cy.get('.oxd-table-row')
-      .contains('Test Skill Name')
+      .contains('Test Licenses Name')
       .parents('.oxd-table-row')
       .within(() => {
         cy.get('.oxd-icon-button').last().click()
@@ -41,7 +40,7 @@ describe('Qualification Skills', () => {
     cy.wait(2000)
     // remove Skill
     cy.get('.oxd-table-row')
-      .contains('Test Skill Name - Edited')
+      .contains('Test Licenses Name')
       .parents('.oxd-table-row')
       .within(() => {
         cy.get('.oxd-icon-button').first().click() // click delete icon
--- cypress/e2e/Admin-Qualification-Membership.cy.js
+++ cypress/e2e/Admin-Qualification-Membership.cy.js
@@ -2,35 +2,34 @@
 
 
 // job
-describe('Qualification Skills', () => {
+describe('Qualification Membership', () => {
 
   beforeEach(() => {
     cy.login('Admin', 'admin123')
   })
 
   it('General', () => {
-    cy.visit('/web/index.php/admin/viewSkills')
-    cy.get('.oxd-text').should('contain', 'Skills')
+    cy.visit('/web/index.php/admin/membership')
+    cy.get('.oxd-text').should('contain', 'Memberships')
     cy.get('.oxd-text').should('be.visible')
   })
 
   // add Skill
-  it('add Skill', () => {
-    cy.visit('/web/index.php/admin/viewSkills')
+  it('add License', () => {
+    cy.visit('/web/index.php/admin/membership')
     cy.get('.oxd-button').click()
-    cy.get('.oxd-input').eq(1).type('Test Skill Name')
-    cy.get('.oxd-textarea').eq(0).type('Test Skill Description')
+    cy.get('.oxd-input').eq(1).type('Test Membership Name')
     cy.get('.oxd-button--secondary').contains('Save').click()
     cy.wait(3000) // wait for the save to complete
   })
 
   // edit & remove Skill
-  it('edit and remove Skill', () => {
-    cy.visit('/web/index.php/admin/viewSkills')
+  it('edit and remove Licenses', () => {
+    cy.visit('/web/index.php/admin/membership')
 
     // edit the Skill
     cy.get('.oxd-table-row')
-      .contains('Test Skill Name')
+      .contains('Test Membership Name')
       .parents('.oxd-table-row')
       .within(() => {
         cy.get('.oxd-icon-button').last().click()
@@ -41,7 +40,7 @@ describe('Qualification Skills', () => {
     cy.wait(2000)
     // remove Skill
     cy.get('.oxd-table-row')
-      .contains('Test Skill Name - Edited')
+      .contains('Test Membership Name')
       .parents('.oxd-table-row')
       .within(() => {
         cy.get('.oxd-icon-button').first().click() // click delete icon
